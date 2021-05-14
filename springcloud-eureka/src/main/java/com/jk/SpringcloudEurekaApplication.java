b248e5907abe18fcd43b03e516262ebe72b58911
liushaofei <764539268@qq.com>
2021-05-14 21:21:57 +0800

    "刘少飞上传的测试项目"

--- springcloud-eureka/src/main/java/com/jk/SpringcloudEurekaApplication.java
+++ springcloud-eureka/src/main/java/com/jk/SpringcloudEurekaApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class SpringcloudEurekaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudEurekaApplication.class, args);
     }
-
 }
